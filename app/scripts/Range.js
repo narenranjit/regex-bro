2c9d49c412fd48c661c537e8e958ba06116c65c6
neocsr <neocsr at someemail.com>
2012-09-29 16:50:17 -0700

    alpha ranges

--- app/scripts/Range.js
+++ app/scripts/Range.js
@@ -1,18 +1,29 @@
 var Range = function(){
 	var charToRange = function(regex){
+		var ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
 		var left = regex.split("-")[0];
 		var right = regex.split("-")[1];
 
 		var range = [];
 		if(!isNaN(+left) && !isNaN(+right)){
-			for(var i=parseInt(left, 10); i<parseInt(right, 10); i++){
+			if(+left > +right){
+				//throw new Error("left indice should be > right");
+				return false;
+			}
+			for(var i=parseInt(left, 10); i<=parseInt(right, 10); i++){
 				range.push(i+"");
 			}
+		}
+		else{
+			range = ALPHABETS.substring(ALPHABETS.indexOf(left), ALPHABETS.indexOf(right)+1).split("");
 		}
 		return range;
 	};
 
 	return{
+		_private: {
+			charToRange: charToRange
+		},
 		match: function(charToMatch, regexChar){
 			var possibilities = charToRange(regexChar);
 			for(var i=0; i< possibilities.length; i++){
---
+++ test/rangeTest.coffee
@@ -0,0 +1,27 @@
+chai = require "chai"
+expect = chai.expect
+
+Range = (require "../app/scripts/Range").Range
+
+describe "Range Tester", ->
+	beforeEach ->
+		@sampleStrings = [
+			"This is d0g b1g"
+		]	
+
+	describe "converts chars to ranges", ->
+		beforeEach ->
+			@cTor = Range._private.charToRange
+
+		it "works with integers", ->
+			expect(@cTor "0-4").to.eql(["0", "1", "2", "3", "4"])
+			expect(@cTor "0-9").to.eql(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+			expect(@cTor "1-5").to.eql(["1", "2", "3", "4", "5"])
+			expect(@cTor "5-5").to.eql(["5"])
+
+		it "does nt work if min > max", ->
+			#expect(@cTor "5-4").to.throw("left indice should be > right")
+
+		it "converts alpha ranges", ->
+			expect(@cTor "a-f").to.eql ["a", "b", "c","d", "e", "f"]
+			expect(@cTor "f-f").to.eql ["f"]
