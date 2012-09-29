de207c0613c2bc6b86bb8f894ca6ee820252a77c
neocsr <neocsr at someemail.com>
2012-09-29 15:58:01 -0700

    removed all trial test libs

--- test/test.js
+++
@@ -1,7 +0,0 @@
-// require(["app/scripts/Range.js"], function(Range) {
-// 	describe("HelloWorldTest", function () {
-// 		it("should return hello world!", function () {
-// 			expect(1).toEqual(2);
-// 		});
-// 	});
-// });
